2413274f19f7a5a8a3f567ae8154abb53bd566a6
Lujano <lugaluchinchilla@gmail.com>
2018-05-22 16:28:12 -0400

    VScode

--- codes/main.cpp
+++ codes/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
 int main(){
     int x = 10;
     int y = 20;
-    std::cout << "Hello World" << std::endl;
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello, World!";
+    std::cout << "Hello, World!";
+    std::cout << "Hello, World!";
+    return 0;
 }
