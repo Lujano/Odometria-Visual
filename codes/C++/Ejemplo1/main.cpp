4be38e01c546d2b879c8bd962fc2d1ddb2e8c91e
Lujano <lugaluchinchilla@gmail.com>
2018-05-25 04:41:50 -0400

    Load Image

--- codes/C++/Ejemplo1/main.cpp
+++ codes/C++/Ejemplo1/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
+using namespace std;
 int main(){
     int x = 10;
     int y = 20;
-    std::cout << "Hello, World!";
-    std::cout << "Hello, World!";
+    cout << "Hello, World!\n";
+    cout << y;
 
     return 0;
 }
---
+++ codes/C++/LoadImage/main.cpp
@@ -0,0 +1,46 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+static void help()
+{
+    cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+            "Usage:\n"
+            "./houghlines <image_name>, Default is ../data/pic1.png\n" << endl;
+}
+
+int main(int argc, char** argv)
+{
+    cv::CommandLineParser parser(argc, argv,
+        "{help h||}{@image|../data/pic1.png|}"
+    );
+    if (parser.has("help"))
+    {
+        help();
+        return 0;
+    }
+    string filename = parser.get<string>("@image");
+    if (filename.empty())
+    {
+        help();
+        cout << "no image_name provided" << endl;
+        return -1;
+    }
+    Mat src = imread(filename, 0);
+    if(src.empty())
+    {
+        help();
+        cout << "can not open " << filename << endl;
+        return -1;
+    }
+
+    imshow("source", src);
+    waitKey();
+
+    return 0;
+}
