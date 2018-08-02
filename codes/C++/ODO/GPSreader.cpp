7783d554f14ccb4ee7d6e788561409fefbf94e98
Lujano <lugaluchinchilla@gmail.com>
2018-08-02 05:44:45 -0400

    reader gps v1

---
+++ codes/C++/ODO/GPSreader.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include "opencv2/core.hpp"
+#include "opencv2/core/types.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+size_t split(const std::string &txt, std::vector<std::string> &strs, char ch);
+
+int main(){
+    ifstream inFile;
+    Mat gps_odometry= Mat::zeros(1, 2, CV_64F);
+    Mat point = Mat::zeros(1, 2, CV_64F);
+    //gps_odometry.push_back(point);
+    inFile.open("00_gt.txt");
+    if (!inFile) {
+        cerr << "Unable to open file datafile.txt";
+        exit(1);   // call system to stop
+    }
+    string x;
+    while (!inFile.eof()) {
+        getline(inFile, x);
+        std::vector<std::string> v;
+        split( x, v, ' ' ); // size 12
+        string a= v[2];
+        //double a = atof(v[3].c_str());
+        //cout<<v[3]<<endl;
+        //point.at<double>(0,1) =  atof(v[3].data()); //x
+        //point.at<double>(0,2) =  atof(v[3].data()); //x
+        gps_odometry.push_back(point);
+        cout<<a<<endl;
+    
+
+    }
+    //cout<<gps_odometry<<endl;
+    inFile.close();
+    return 0;
+}
+
+size_t split(const std::string &txt, std::vector<std::string> &strs, char ch)
+{
+    size_t pos = txt.find( ch );
+    size_t initialPos = 0;
+    strs.clear();
+
+    // Decompose statement
+    while( pos != std::string::npos ) {
+        strs.push_back( txt.substr( initialPos, pos - initialPos ) );
+        initialPos = pos + 1;
+
+        pos = txt.find( ch, initialPos );
+    }
+
+    // Add the last one
+    strs.push_back( txt.substr( initialPos, std::min( pos, txt.size() ) - initialPos + 1 ) );
+
+    return strs.size();
+}
+
+// g++ -g -o GPSreader.out GPSreader.cpp `pkg-config opencv --cflags --libs`
